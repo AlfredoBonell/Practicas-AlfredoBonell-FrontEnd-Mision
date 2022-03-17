7fcbf95b0254d4356074d559a94d4f9f8f296c53
Alfredo Bonell <99234057+AlfredoBonell@users.noreply.github.com>
2022-03-16 21:30:59 -0700

    Semana 4 - Js
    - Se agregan más campos a la Pokedex.

--- Semana 4 - Js/pokedex/js/script.js
+++ Semana 4 - Js/pokedex/js/script.js
@@ -55,6 +55,7 @@ $(function(){
         timeout = setTimeout(function(){
             $("div.alert-success").css("display", "block");
         }, 300);
+        $("div.main").css("display", "block");
     }
     
     function mostrarMensajeAdvertencia(){
@@ -62,6 +63,7 @@ $(function(){
         timeout = setTimeout(function(){
             $("div.alert-warning").css("display", "block");
         }, 300);
+        $("div.main").css("display", "block");
     }
 
     function mostrarMensajeError(){
@@ -69,6 +71,7 @@ $(function(){
         timeout = setTimeout(function(){
             $("div.alert-danger").css("display", "block");
         }, 300);
+        $("div.main").css("display", "block");
     }
 
     function mostrarDittoError(){       
@@ -85,7 +88,7 @@ $(function(){
             $("div.alert-success").css("display", "none");
             $("div.alert-warning").css("display", "none");
             $("div.alert-danger").css("display", "none");
-        }, 3000);
+        }, 5000);
     }
 
     $("input#buscar-pokemon").on('keypress', function(e){
@@ -103,8 +106,10 @@ $(function(){
                             $("div.card-body").empty();
                             // console.log(data);
                             let imagenPokemon = data['sprites'].front_shiny;
-                            let poke_types = data.types.map(type => type.type.name);
-                            let type = main_types.find(type => poke_types.indexOf(type) > -1);
+                            let tipo = data.types.map(type => type.type.name).join(', ');
+                            let habilidad = data.abilities.map(type => type.ability.name).join(', ');
+                            let movimientos = data.moves.slice(0, 3).map(type => type.move.name).join(', ');
+                            let type = main_types.find(type => tipo.indexOf(type) > -1);
                             let nombrePokemon = data.name[0].toUpperCase() + data.name.slice(1);
                             let color = colors[type];
                             console.log(color);
@@ -113,9 +118,22 @@ $(function(){
                                 <h5 class="card-title">${nombrePokemon} 
                                     <span class="number">#${data.id.toString().padStart(3, '0')}</span>
                                 </h5>
-                                <small class="type">Tipo: <span>${type}</span></small>
-                                <p class="card-text"></p>
+                                <small class="type">Tipo: <a href="#" class="badge badge-primary">${tipo}</a></small><br>
+                                <small class="type">Debilidad: <a href="#" class="badge badge-info">${habilidad}</a></small><br>
+                                <small class="type">Movimientos: <a href="#" class="badge badge-success">${movimientos}</a></small><br><br>
                             `;
+                            //Se saca la información del pokemón, atk, hp, etc.
+                            $.each(data['stats'], function(index, value){
+                                // console.log(index, value);
+                                append += `
+                                <div style="display: flex; justify-content: space-between; align-content: space-between; padding: 5px; font-size: 18px;">
+                                    <p>${value['stat'].name[0].toUpperCase() + value['stat'].name.slice(1)}</p>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: ${value.base_stat}%;" aria-valuenow="${value.base_stat}" aria-valuemin="0" aria-valuemax="100">${value.base_stat}%</div>
+                                </div>
+                                `;
+                            })
                             $("div.card-body").css("background", color);
                             $("div.card-body").append(append);
                         }
@@ -131,6 +149,7 @@ $(function(){
         if ($(this).val() == '') {
             $("#contenedor-pokemones").empty();
             $("div.card-body").css("background", "#FFF");
+            $("div.main").css("display", "block");
         }
     });
 });
